54c3b45978825023034b70d9f25d494a691559e7
EDJ <jinende1997@live.com>
2016-05-15 23:30:58 +0800

    No more abstract

--- Compiling.hs
+++ Compiling.hs
@@ -1,91 +1,124 @@
 import Register
 import Internal
 
-data ICi = ICi {operation :: ICop, linkages :: [(Cdata, Cdata)], using :: [Register]}
+import Data.List (nub, concat)
+
+randomname :: String -> String
+
+data ICi = ICi {operation :: ICop, linkages :: [(Cdata,Cdata)] ,using :: [Register]}
 
 
-compileList :: [SStruc] -> [ICop]
-compileList = map compile
+nameGenerator :: [SStruc] -> String
+nameGenerator = concat . map nameGenerator'
+where nameGenerator' :: SStruc -> String
+      nameGenerator' (SAtom x) = x
+      nameGenerator' (SString x) = x
+      nameGenerator' (SQuote x) = nameGenerator' x
+      nameGenerator' (SList l) = nameGenerator l
+      nameGenerator' (SBool x) = show x
+      nameGenerator' (SNum x) = show x
+
+compileList :: [SStruc] -> ICi
+compileList =integrateICi . map compile
   where opt1 :: [ICop] -> [ICop]
-        opt1 ((Run (CLabel x)):rem) = (opt1 x) ++ (opt1 rem)
-        opt1 ((Assign2 y (CLabel x)):rem) = (Assign2 y (CLabel (opt1 x))):(opt1 rem) 
-        opt1 (x:rem) = x : (opt1 rem)
+--        opt1 ((Run (CLabel x)):rem) = (opt1 x) ++ (opt1 rem)
+--        opt1 ((Assign2 y (CLabel x)):rem) = (Assign2 y (CLabel (opt1 x))):(opt1 rem) 
+--        opt1 (x:rem) = x : (opt1 rem)
+        integrateICi :: [ICi] -> ICi
+        integrateICi icis =
+          ICi (Run (CLabel (map . operation $ icis))) (concat. map. linkages $ icis)
+          (nub. concat. map. using $ icis) 
 
+        
 compile :: SStruc -> ICi
-compile (SString x) = Assign2 Val (CString x)
-compile (SNum x) = Assign2 Val (CInt x)
-compile (SBool x) = Assign2 Val (CBool x)
-compile (SQuote x) = Assign2 Val (CQuote x)
-compile (SAtom x) = LookVar Val (CAtom x)
+compile (SString x) =ICi (Assign2 Val (CString x)) [] [Val]
+compile (SNum x) =ICi (Assign2 Val (CInt x)) [] [Val]
+compile (SBool x) =ICi (Assign2 Val (CBool x)) [] [Val]
+compile (SQuote x) =ICi (Assign2 Val (CQuote x)) [] [Val]
+compile (SAtom x) =ICi (LookVar Val (CAtom x)) [] [Val]
 
 compile (SList ((SAtom "define"):(SAtom x):body:[])) =
-  Run (CLabel [(compile body), 
-               DefVar (CAtom x) Val])  
+  (ICi (Run (CLabel [(compile body), 
+                     DefVar (CAtom x) Val])) [] [Val])
+                        
 compile (SList ((SAtom "define"):(SList ((SAtom funcName):args)):body:[])) =
- compile (SList ((SAtom "define"):(SAtom funcName):(SList (SAtom "lambda"):(SList args):body)))
+  compile (SList ((SAtom "define"):(SAtom funcName):(SList (SAtom "lambda"):(SList args):body)))
 
 
-compile (SList ((SAtom "lambda"):argandbody)) =
-  Assign2 Val (CLabel (compileLambda' argandbody))
+compile (SList ((SAtom "lambda"):(SList arg):body))@all =
+  let name = nameGenerator all
+  in ICi
+     (Assign2 Val (CExItem name))
+     [(CExItem name,
+       CLabel (compileLambda' ((SList (reverse arg)):body)))]
+     [Val]
   where compileLambda' :: [SStruc] -> ICop 
         compileLambda' (SList ((SAtom arg): args):body) =
-          Run (CLabel [(Pull Argl Val),
+          Run (CLabel [(Pop Argl Val),
                        (DefVar (CAtom arg) Val),
                        (compileLambda' ((SList args):body))])
         compileLambda' ((SList []):body) =
           Run (CLabel (compileList body))
 
-
-compile (SList ((SAtom "cons"):a:b:[])) =
-  Run (CLabel [(compile a),
-               (Push Argl Val),
-               (compile b),
-               (Push Argl Val),
-               (compile (SAtom "CONS")),
-               (Call Val)])
-  
-compile (SList ((SAtom "car"):a:[])) =
-  Run (CLabel [(compile a),
-               (Push Argl Val),
-               (compile (SAtom "CAR")),
-               (Call Val)])
-
-compile (SList ((SAtom "cdr"):a:[])) =
-  Run (CLabel [(compile a),
-               (Push Argl Val),
-               (compile (SAtom "CDR")),
-               (Call Val)])
-
 compile (SList ((SAtom "if"):pred:branch1:branch2)) =
-  Run (CLabel [(compile pred),
-               (TestGo Val (Label (compileList branch1))
-                (Label (compileList branch2)))])
+  ICi (Run (CLabel [(compile pred),
+                    (TestGo Val (CLabel (compileList branch1))
+                     (CLabel (compileList branch2)))])) [] [Val]
 
 compile (SList ((SAtom "set!"):(SAtom var):val:[])) =
-  Run (CLabel [(compile val),
-               (SetVar (CAtom var) Val)])
+  ICi (Run (CLabel [(compile val),
+                    (SetVar (CAtom var) Val)])) [] [Val]
 
-compile (SList (func:arg:args)) = Run (CLabel [(compile arg),
-                                               (Push Argl Val),
-                                               (compile (SList (func:args)))])
-  
-compile (SList (func:[])) = Run (CLabel [(compile func),
-                                         (Call Val)])
+compile (SList (func:arg:args)) =
+  ICi (Run (CLabel [(compile arg),
+                    (Push Argl Val),
+                    (compile (SList (func:args)))])) [] [Argl]
 
 
+compile (SList ((SAtom "cons"):[])) = ICi (Run (CLabel [Assign2 Val (CExItem "CONS"),
+                                                        Call Val])) [] [Val]
 
-addCast :: String -> String ->String
-addCast cast var = "(" ++ (cast ++ (")" ++ var))
+compile (SList ((SAtom "car"):[])) = ICi (Run (CLabel [Assign2 Val (CExItem "CAR"),
+                                                       Call Val])) [] [Val]
 
-addParenthesis :: String -> String
-addParenthesis a = '(' : (a ++ ")")
-
-
-toC :: ICop -> String
+compile (SList ((SAtom "cdr"):[])) = ICi (Run (CLabel [Assign2 Val (CExItem "CDR"),
+                                                       Call Val])) [] [Val]
 
 
-toC' (Assign1 a b) = (show a) ++ ("=" ++ (show b))
-toC' (Assign2 a b) = (show a) ++ ("=" ++ (addCast "ptlong" (show b)))
-toC' (Push ra rb) =
-  "*" ++ (addParenthesis (addParenthesis (addCast "ptlong*" (show ra))) ++ "++") ++ "=" ++(show rb)
-toC' (Pull ra rb) =  
+
+compile (SList (func:[])) =
+  ICi (Run (CLabel [(compile func),
+                    (Call Val)])) [] [Val]
+
+
+
+addcall :: String -> [String] -> String
+addcall func (arg:args) = func ++ "(" ++ arg ++ (sepbyp args) ++ ")"
+  where sepbyp [] = ""
+        sepbyp args = concat . map (',':) $ args
+
+
+assignmentsentence :: String -> String -> String
+assignmentsentence to from = to ++ "=" ++ from ++ ";"
+
+
+
+optoC :: ICop -> String
+optoC (Run (CLabel x)) = concat . map optoC $ x
+optoC (Assign1 a b) = assignmentsentence (show a) (addcall "(ptlong)" [show b])
+optoC (Assign2 a b) = assignmentsentence (show a) (addcall "(ptlong)" [show b])
+optoC (Push a b) =
+  assignmentsentence
+  (addcall "*" [addcall "(ptlong*)" [(show a) ++ "++"]])
+  (show b)
+
+
+
+
+
+
+
+
+
+
+
--- Register.hs
+++ Register.hs
@@ -9,13 +9,27 @@ data Cdata =
   | CQuote String
   | CAtom String
   | CLabel [ICop]
-  | CExItem String
+  | CExItem String Cdata
+
+
+addcall :: String -> [String] -> String
+addcall func (arg:args) = func ++ "(" ++ arg ++ (sepbyp args) ++ ")"
+  where sepbyp args = concat . map (',':) $ args
+
+
+instance (Show Cdata) where
+  show (CString a) = "\"" ++ a ++ "\""
+  show (CAtom a) = addcall "ATOM" [show (CString a)]
+  show (CQuote a) = addcall "QUOTE" [show (CString a)]
+  show (CInt a) = show a
+  show (CBool a) = show a
+  show (CExItem a _) = show a
+
 data ICop =
   Assign1 Register Register
   | Assign2 Register Cdata 
   | Push Register Register
   | Pop Register Register
-  | Pull Register Register
   | Run Cdata 
   | Call Register
   | TestGo Register Cdata Cdata
